a69f5822425eb243ab91f7afb981d8dc65c3c895
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-07-25 13:04:17 -0600

    addmentor-component

--- app/components/App.js
+++ app/components/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
+import AddMentor from './AddMentor';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mentors: [],
+      students: [],
+    }
+
+    this.updateMentors = this.updateMentors.bind(this);
+  }
+
+  updateMentors(mentors) {
+    this.setState({ mentors: [mentors]})
+    console.log(this.state);
+  }
+
 
   render() {
     return (
@@ -10,6 +26,7 @@ export default class App extends Component {
           We are going to now talk to the GitHub API. Ready?
           <a href='/authenticate'>CLICK ME!</a>
         </p>
+        <AddMentor updateMentors={this.updateMentors} />
       </div>
     )
   }
