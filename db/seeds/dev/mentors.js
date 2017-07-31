00ff57609080d5a59ab4ddc6a10538c0858c6b3a
John Binning <john.binning17@gmail.com>
2017-07-31 10:26:47 -0600

    add fix for mentor seeds

--- db/seeds/dev/mentors.js
+++ db/seeds/dev/mentors.js
@@ -1,10 +1,12 @@
 const mentors = require('../seedData/mentors.js');
 
 const createMentor = (knex, mentor) => {
-  const { gh_id, preferred_name, slack, email, location, phone, accepting_new, availability, company, position, dev_type, stack, pairing_location, avatar_url } = mentor;
+  const { gh_id, preferred_name, slack, email, location, phone, accepting_new, availability, company, position, dev_type, stack, pairing_location, avatar_url, preferred_contact, bio } = mentor;
   return knex('mentors').insert({
     gh_id,
     preferred_name,
+    preferred_contact,
+    bio,
     slack,
     email,
     location,
