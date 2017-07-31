c2a24d53c7f79d3752394d9e007b84fdaa87ee7f
Leta Keane <letakeane+turing@gmail.com>
2017-07-31 17:00:34 -0600

    Conditionally do a POST or PATCH when creating/editing a mentor

--- app/components/MentorHome.js
+++ app/components/MentorHome.js
@@ -3,12 +3,34 @@ import { Route } from 'react-router';
 import { Link } from 'react-router-dom';
 import MentorCard from './MentorCard';
 
-export const MentorHome = ({ currentMentor }) => {
+export default class MentorHome extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mentor: {}
+    }
+  }
 
-  return (
-    <div>
-      <MentorCard mentor={currentMentor} />
-      <Link to='/edit-mentor'>EDIT PROFILE</Link>
-    </div>
-  )
+  getMentor() {
+    const { user } = this.props;
+    fetch(`/api/v1/mentors/${user.ghId}`)
+    .then(resp => resp.json())
+    .then(mentors => {
+      console.log('fetch results: ', mentors);
+      this.setState({ mentor: mentors[0] })
+    })
+  }
+
+  componentWillMount() {
+    this.getMentor();
+  }
+
+  render() {
+    return (
+      <div>
+        <MentorCard mentor={this.state.mentor || this.props.currentMentor} />
+        <Link to='/edit-mentor'>EDIT PROFILE</Link>
+      </div>
+    )
+  }
 }
