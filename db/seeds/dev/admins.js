526feeae90005b3e2176d292a6e2c31eb6005c2c
John Binning <john.binning17@gmail.com>
2017-07-31 13:55:04 -0600

    Add admin schema

---
+++ db/seeds/dev/admins.js
@@ -0,0 +1,23 @@
+const admins = require('../seedData/admins.js');
+
+const createAdmin = (knex, admin) => {
+  const { gh_id, preferred_name, avatar_url } = admin;
+  return knex('admins').insert({
+    gh_id,
+    preferred_name,
+    avatar_url
+  }, 'id');
+};
+
+exports.seed = (knex, Promise) => {
+  return knex('admins').del()
+    .then(() => {
+      const adminPromises = [];
+      admins.forEach((admin) => {
+        adminPromises.push(createAdmin(knex, admin));
+      });
+
+      return Promise.all(adminPromises);
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
---
+++ db/seeds/seedData/admins.js
@@ -0,0 +1,29 @@
+const admins = [
+  {
+    gh_id: 15134381,
+    preferred_name: 'Allison Reu Singer',
+    avatar_url: 'https://avatars.githubusercontent.com/u/15134381?'
+  }
+  // {
+  //   gh_id: 24737239,
+  //   preferred_name: 'John Binning',
+  //   avatar_url: 'https://avatars.githubusercontent.com/u/24737239?',
+  // }
+  // {
+  //   gh_id: 22563791,
+  //   preferred_name: 'Leta Keane',
+  //   avatar_url: 'https://avatars.githubusercontent.com/u/22563791?',
+  // }
+  // {
+  //   gh_id: 23202538,
+  //   preferred_name: 'Spencer Hilvitz',
+  //   avatar_url: 'https://avatars.githubusercontent.com/u/23202538?',
+  // }
+]
+
+module.exports = admins;
+
+//  table.increments('id').primary();
+//       table.integer('gh_id');
+//       table.string('preferred_name');
+//       table.string('avatar_url');
