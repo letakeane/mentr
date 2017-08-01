8128ab767388a8d87fc602604eb13dcbb8ba6563
Leta Keane <letakeane+turing@gmail.com>
2017-08-01 10:06:01 -0600

    Edit seed location in production

--- knexfile.js
+++ knexfile.js
@@ -43,7 +43,7 @@ module.exports = {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/production'
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
