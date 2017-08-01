81ca982b40a4072e4606580d1255edd685d2848c
Leta Keane <letakeane+turing@gmail.com>
2017-08-01 10:22:07 -0600

    Add heroku webpack config file

---
+++ webpack.prod.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: '#source-map',
+  context: __dirname,
+   entry: [
+     './app/index.js'
+   ],
+   output: {
+     path: __dirname,
+     filename: 'bundle.js',
+     publicPath: '/'
+   },
+   module: {
+     loaders: [{
+       test: /.jsx?$/,
+       loader: 'babel-loader',
+       include: path.join(__dirname, 'app'),
+       exclude: /node_modules/,
+       query: {
+         presets: ['es2015', 'react']
+       }
+     },
+    { test: /\.css$/, loader: "style-loader!css-loader" }
+    ]
+   },
+   node: {
+     fs: 'empty'
+   },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.scss', '.css']
+  }
+};
