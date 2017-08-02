9b49d3e91300f3d60d26656b152f5254249e1d85
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-08-01 18:09:28 -0600

    change create-student path

--- app/components/ChooseStatus.js
+++ app/components/ChooseStatus.js
@@ -7,7 +7,7 @@ export const ChooseStatus = (props) => {
     <div>
       <h2>I AM A:</h2>
       <div className='choose-link-box'>
-        <Link to='/create-student' className='button' className='choose-link'>STUDENT</Link>
+        <Link to='/edit-student' className='button' className='choose-link'>STUDENT</Link>
         <Link to='/edit-mentor' className='choose-link'>MENTOR</Link>
       </div>
     </div>
